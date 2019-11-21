8d708fdb3cc862fce96b3c1383c5f8ed55294e8f
Dartnimus <57891573+Dartnimus@users.noreply.github.com>
2019-11-21 15:39:16 +0200

    добавлены новые локаторы на форму регистации и логина, добавлена проверка урла на наличие логина (т.е. правильный ли урл) добавлен переход на страницу с логином

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def go_to_login_page(browser):
@@ -10,6 +11,7 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_login_link()      # проверяем существование ссылки ведущей к логину
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-
+    #page.should_be_login_link()      # проверяем существование ссылки ведущей к логину
+    #page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
