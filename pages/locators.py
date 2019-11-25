88e0e564118a9c243240bb184ec901ec1a73e09f
Dartnimus <57891573+Dartnimus@users.noreply.github.com>
2019-11-25 12:28:15 +0200

    изменили класс ProductPageБ что бы проверка не зависела от типа товара т.е. любой товар по любой ссылке проверяется так же добавили параметризацию, на проверку 10 страниц

--- pages/locators.py
+++ pages/locators.py
@@ -9,6 +9,15 @@ class LoginPagesLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators():
+    BASKET_TOTAL = (By.XPATH, "//div[strong[text()='Basket total:']]")
     ADD_TO_BASKED_BTN = (By.CLASS_NAME,"btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+    PRODUCT_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
+    ADD_TO_BASKET_MSG = (
+    By.XPATH, '//div[strong[text()="{0}"] and normalize-space(text()="has been added to your basket.")]')
+    VIEW_BASKET_BTN = (By.LINK_TEXT, 'View basket')
+    CHECKOUT_NOW_BTN = (By.LINK_TEXT, 'Checkout now')
+    BASKET_MSG = (By.XPATH, '//p[normalize-space(text()="Your basket total is now") and strong[text()="{0}"]]')
+
 
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+
 
 
 class ProductPage(BasePage):
@@ -9,3 +11,28 @@ class ProductPage(BasePage):
 
     def check_product_page(self): # проверка
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKED_BTN), '"Add to basket" button not found'
+
+
+    def get_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        return product_name
+
+    def get_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        return product_price
+
+    def check_add_to_basket_message(self, name):
+        assert self.is_element_present(ProductPageLocators.ADD_TO_BASKET_MSG[0],
+                                       ProductPageLocators.ADD_TO_BASKET_MSG[1].format(name)), \
+            'Added to basket message is not displayed'
+
+    def check_basket_total(self, price):
+        current_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert current_price.startswith(f'Basket total: {price}'), f'{current_price} != {price}. Should be the same'
+
+    def check_basket_notification(self, price):
+        assert self.is_element_present(*ProductPageLocators.VIEW_BASKET_BTN), '"View basket" button not found'
+        assert self.is_element_present(*ProductPageLocators.CHECKOUT_NOW_BTN), '"Checkout now" button not found'
+        assert self.is_element_present(ProductPageLocators.BASKET_MSG[0],
+                                       ProductPageLocators.BASKET_MSG[1].format(price)), \
+            'Basket status message is wrong'
